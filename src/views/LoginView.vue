6970c568675c7c78f8ae8938cbd4d3cea1a90c67
lipk <lipk@mingyuanyun.com>
2023-05-30 07:07:35 +0800

    feat: 初始化项目

---
+++ src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <a-button type="primary">Primary Button</a-button>
+    <a-button>Default Button</a-button>
+    <a-button type="dashed">Dashed Button</a-button>
+    <a-button type="text">Text Button</a-button>
+    <a-button type="link">Link Button</a-button>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss"></style>
